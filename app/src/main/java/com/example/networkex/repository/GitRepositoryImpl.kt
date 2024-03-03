62812d99930958f5dfbd67e5725944d700fb77b0
이정현 <cw03125@openobject.net>
2024-03-03 16:10:43 +0900

    repository api 요청 중복 수정

--- app/src/main/java/com/example/networkex/repository/GitRepositoryImpl.kt
+++ app/src/main/java/com/example/networkex/repository/GitRepositoryImpl.kt
@@ -4,16 +4,20 @@ import android.util.Log
 import com.example.networkex.model.GithubResponse
 import com.example.networkex.network.GitApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import okhttp3.ResponseBody
 import retrofit2.Response
 
 class GitRepositoryImpl(private val gitApi: GitApi) : GitRepository {
-    override suspend fun getUsers(q: String): Flow<Response<GithubResponse>> = flow {
+    override fun getUsers(q: String): Flow<Response<GithubResponse>> = flow {
         val response = gitApi.getUsers(q)
         if (response.isSuccessful) {
-            emit(gitApi.getUsers(q))
+            emit(response)
         } else {
             Log.e("Impl", response.errorBody().toString())
         }
+    }.catch {
+        emit(Response.error(400, ResponseBody.create(null, "")))
     }
 }
